e8e3d14561594dc22efda2516145674021101b18
reinib <brent.reininger89@gmail.com>
2019-07-26 14:00:26 -0700

    Create and edit Movies

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required] public Genre Genre { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
        
     }
